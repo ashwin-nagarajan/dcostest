ad305e9f9e2c55ea5cb78c7a278d7efcd917a079
cirano <sn.ashwin@scoopwhoop.com>
2017-09-06 12:50:08 +0530

    adding another test revision

--- nodedcos/routes/index.js
+++ nodedcos/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express for the pipeline ' });
+  res.render('index', { title: 'Express for the pipeline:1.0 ' });
 });
 
 module.exports = router;
