b761094c892baa288d1190ff701fdccfe41338c2
cirano <sn.ashwin@scoopwhoop.com>
2017-10-12 11:42:48 +0530

    adding version 4

--- nodedcos/routes/index.js
+++ nodedcos/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express for the pipeline:3.0 ' });
+  res.render('index', { title: 'Express for the pipeline:4.0 ' });
 });
 
 module.exports = router;
